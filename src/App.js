fc98d0b7712d2a051c402ef63bbdb1053e2e9ee0
marcusloy77 <marcus.loy.77@gmail.com>
2022-10-03 16:10:01 +1100

    idk

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import './components/feed.scss';
+import './components/nav.scss'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
@@ -9,7 +10,7 @@ import { MdOutlineNotificationsNone } from 'react-icons/md'
 import { RiFileList2Line } from 'react-icons/ri'
 import { CgMoreO } from 'react-icons/cg'
 
-let tweets = [{name: 'Sam Larew', tweet: 'Roddy rich fell off', likes : 802}, {name: 'Modern Warzone', tweet: 'Twitch streamer blablabla', likes : 802}, {name: 'FaZe Jsmooth', tweet: 'Clout be wilding', likes : 123}]
+let tweets = [{dispName: 'Sam Larew', username : 'samLar', tweet: 'Roddy rich fell off', likes : 802, comments : '21', retweets : '84', timeUploaded : 1200, dispPictureSRC: './images/idk'},{dispName: 'jack Welsh', username : 'jackwel', tweet: 'Marcus is a great dev please hire him', likes : 100802, comments : '20211', retweets : '111184', timeUploaded : 1200, dispPictureSRC: './images/idk'}]
 let navItems = ['Home', 'Explore', 'Notifications', 'Messages', 'Messages', 'Bookmarks', 'Lists', 'Profile', 'More']
 
 
@@ -22,14 +23,14 @@ function App() {
           <Nav defaultActiveKey="/home" className="flex-column">
             <Container>
               <div className='navWrap logo'><BsTwitter size='1.8em' color='rgb(29, 155, 240)'></BsTwitter></div>
-              <div className='navWrap' style={{width: navItems[0].length * 37 +'px'}}><Nav.Item className='navElem'><span className='navIcons homeIcon'><AiFillHome size='1.2em'></AiFillHome></span> <span className='navWords'>Home</span></Nav.Item></div>
-              <div className='navWrap' style={{width: navItems[1].length * 24 +'px'}}><Nav.Item className='navElem'><span className='navIcons explIcon'><BsHash size='1.6em'></BsHash></span><span className='navWords'>Explore</span></Nav.Item></div>
-              <div className='navWrap' style={{width: navItems[2].length * 17 +'px'}}><Nav.Item className='navElem'><span className='navIcons notiIcon'><MdOutlineNotificationsNone size='1.6em'></MdOutlineNotificationsNone></span><span className='navWords'>Notifications</span></Nav.Item></div>
-              <div className='navWrap' style={{width: navItems[3].length * 24 +'px'}}><Nav.Item className='navElem'><span className='navIcons messIcon'><AiOutlineMail size='1.4em'></AiOutlineMail></span><span className='navWords'>Messages</span></Nav.Item></div>
-              <div className='navWrap' style={{width: navItems[4].length * 25 +'px'}}><Nav.Item className='navElem'><span className='navIcons bookIcon'><BsBookmark size='1.2em'></BsBookmark></span><span className='navWords'>Bookmarks</span></Nav.Item></div>
-              <div className='navWrap' style={{width: navItems[5].length * 15 +'px'}}><Nav.Item className='navElem'><span className='navIcons listIcon'><RiFileList2Line size='1.2em'></RiFileList2Line></span><span className='navWords'>Lists</span></Nav.Item></div>
-              <div className='navWrap' style={{width: navItems[6].length * 31 +'px'}}><Nav.Item className='navElem'><span className='navIcons profIcon'><BsPerson size='1.4em'></BsPerson></span><span className='navWords'>Profile</span></Nav.Item></div>
-              <div className='navWrap' style={{width: navItems[7].length * 21 +'px'}}> <Nav.Item className='navElem'><span className='navIcons moreIcon'><CgMoreO size='1.2em'></CgMoreO></span><span className='navWords'>More</span></Nav.Item></div>
+              <div className='hoverWrap'><div className='navWrap' style={{width: navItems[0].length * 38 +'px'}}><Nav.Item className='navElem'><span className='navIcons homeIcon'><AiFillHome size='1.2em'></AiFillHome></span> <span className='navWords'>Home</span></Nav.Item></div></div>
+              <div className='hoverWrap'><div className='navWrap' style={{width: navItems[1].length * 24 +'px'}}><Nav.Item className='navElem'><span className='navIcons explIcon'><BsHash size='1.6em'></BsHash></span><span className='navWords'>Explore</span></Nav.Item></div></div>
+              <div className='hoverWrap'><div className='navWrap' style={{width: navItems[2].length * 17 +'px'}}><Nav.Item className='navElem'><span className='navIcons notiIcon'><MdOutlineNotificationsNone size='1.6em'></MdOutlineNotificationsNone></span><span className='navWords'>Notifications</span></Nav.Item></div></div>
+              <div className='hoverWrap'><div className='navWrap' style={{width: navItems[3].length * 24 +'px'}}><Nav.Item className='navElem'><span className='navIcons messIcon'><AiOutlineMail size='1.4em'></AiOutlineMail></span><span className='navWords'>Messages</span></Nav.Item></div></div>
+              <div className='hoverWrap'><div className='navWrap' style={{width: navItems[4].length * 26 +'px'}}><Nav.Item className='navElem'><span className='navIcons bookIcon'><BsBookmark size='1.2em'></BsBookmark></span><span className='navWords'>Bookmarks</span></Nav.Item></div></div>
+              <div className='hoverWrap'><div className='navWrap' style={{width: navItems[5].length * 16 +'px'}}><Nav.Item className='navElem'><span className='navIcons listIcon'><RiFileList2Line size='1.2em'></RiFileList2Line></span><span className='navWords'>Lists</span></Nav.Item></div></div>
+              <div className='hoverWrap'><div className='navWrap' style={{width: navItems[6].length * 32 +'px'}}><Nav.Item className='navElem'><span className='navIcons profIcon'><BsPerson size='1.4em'></BsPerson></span><span className='navWords'>Profile</span></Nav.Item></div></div>
+              <div className='hoverWrap'><div className='navWrap' style={{width: navItems[7].length * 21 +'px'}}> <Nav.Item className='navElem'><span className='navIcons moreIcon'><CgMoreO size='1.2em'></CgMoreO></span><span className='navWords'>More</span></Nav.Item></div></div>
               <div className='tweetBtnNav'><Nav.Item className='tweetBtn'> <div className='tweetBtnNavInner'><strong>Tweet</strong></div>{' '}</Nav.Item></div>
             </Container>
           </Nav>
@@ -48,7 +49,15 @@ function App() {
           </div>
           <div className='tweets'>{tweets.map((tweet, index) => {
             return (
-              <div className='bigTweet' key={index}><div className='tweetName tweet'><strong>{tweet.name}</strong></div> <div className='tweetContent tweet'>{tweet.tweet}</div><div className='tweetLikes tweet'>{tweet.likes}</div></div>
+              <div className='bigTweet' key={index}>
+                <div className='tweetName tweet'>
+                  <strong>{tweet.dispName}</strong> <p>@{tweet.username}</p>
+                </div> 
+                <div className='tweetContent tweet'>{tweet.tweet}</div>
+                <div className='tweetComment tweet'>{tweet.comments}</div>
+                <div className='tweetRetweets tweet'>{tweet.retweets}</div>
+                <div className='tweetLikes tweet'>{tweet.likes}</div>
+              </div>
             )
           })}</div>
         </div>
